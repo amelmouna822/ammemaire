5ee77bec32ea38b66e27142c6d0290145e5952fe
amelmouna822 <amelmouna199@gmail.com>
2025-04-30 23:20:08 +0200

    html

--- app.py
+++ app.py
@@ -8,10 +8,32 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return render_template("index.html")  # صفحة الترحيب
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/Jobs")
+def Jobs():
+    return render_template("Jobs.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 @app.route("/home")
 def home():
     return render_template("home.html")  # الصفحة الرئيسية
+@app.route("/login_job_seeker")
+def login_job_seeker():
+    return render_template("login_job_seeker.html")
+
+
+@app.route("/login_company")
+def login_company():
+    return render_template("login_company.html")
+
 
 @app.route("/job_seekers", methods=["GET", "POST"])
 def job_seekers():
